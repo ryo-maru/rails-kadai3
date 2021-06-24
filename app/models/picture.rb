78e00bada04774ac2abb1b92ab9aef075ebb684d
ryo maruyama <t.t.uncle0803@gmail.com>
2021-06-25 02:10:23 +0900

    hogehpge

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,6 @@
 class Picture < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
-  validates :name, presence: true
-  validates :email, presence: true
   validates :content, presence: true
   validates :picture, presence: true
 end
