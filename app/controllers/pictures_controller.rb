f5659bb5eb3be1ade30a5ad70335fcd2cf8ab2fa
ryo maruyama <t.t.uncle0803@gmail.com>
2021-06-25 14:34:48 +0900

    投稿エラーの修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -21,6 +21,7 @@ class PicturesController < ApplicationController
 
   def confirm
     @picture = Picture.new(picture_params)
+    render :new if @picture.invalid?
   end
 
   # GET /pictures/1/edit
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,7 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-  process resize_to_fit: [100, 100]
+  
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
